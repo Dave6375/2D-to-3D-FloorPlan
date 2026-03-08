afed80193e61c98c321f9a862362ef9b2c1bbdd1
Dave6375 <idokod09@gmail.com>
2026-03-08 12:48:01 -0500

    implemeted project card ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import {ArrowRight, Clock, GemIcon, Layers} from "lucide-react";
+import Button from "../../components/Button";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -11,22 +13,93 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-      <div className='home'>
-        <Navbar />
+    <div className="home">
+      <Navbar />
 
-          <section className='hero'>
-              <div className='announce'>
-                  <div className='dot'>
-                      <div className='pulse'></div>
-                  </div>
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
 
-                  <p>Introducing Simplex 2D to 3D Platform</p>
+          <p>Introducing Simplex 2D to 3D Platform</p>
+        </div>
 
-                  <h1>Build beautiful spaces at the speed of though with Simplex</h1>
+        <h1>Build beautiful spaces at the speed of thought with Simplex</h1>
 
+        <p className="subtitle">
+          Simplex is an AI-first design environment that helps users create and visualize, render, and
+          ship architectural projects faster than ever
+        </p>
 
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Get Started <GemIcon className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
               </div>
-          </section>
-      </div>
-  )
+
+              <h2>Upload your illustration</h2>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+
+            <p>Upload images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+            <div className='section-head'>
+                <div className='copy'>
+                    <h2>Projects</h2>
+                    <p>Your latest work and shared community projects, all in one place.</p>
+                </div>
+            </div>
+
+            <div className='projects-grid'>
+                <div className='project-card group'>
+                    <div className='preview'>
+                        <img
+                            src='https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png' alt='project preview' />
+
+                        <div className='badge'>
+                            <span>Community</span>
+                        </div>
+                    </div>
+
+                    <div className='card-body'>
+                        <div>
+                            <h3>Project Manhattan</h3>
+
+                            <div className='meta'>
+                                <Clock size={12} />
+                                <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                                <span>By Simplex</span>
+                            </div>
+                        </div>
+
+                        <div className='arrow'>
+                            <ArrowRight size={16} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+      </section>
+    </div>
+  );
 }
